f785fe0ef15044bf6a4fd7d6ee86422101aad127
Dan Tasse <dan.tasse@gmail.com>
2015-03-17 22:20:57 -0400

    first version, wordnik scrape script working

---
+++ README.txt
@@ -0,0 +1,9 @@
+Pebble watch apps for all the three letter scrabble words.
+
+Three letter word list (from TWL3): http://www.isc.ro/nl/commands/lists.html
+
+(Differences between different lists: http://www.word-buff.com/scrabble-word-list.html)
+
+Definitions from Wordnik, via python client:
+https://github.com/wordnik/wordnik-python
+
---
+++ scrape_wordnik.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from wordnik import WordApi, swagger
+import ConfigParser, argparse, time, csv
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start_at', default='aaa')
+    parser.add_argument('--words_file', default='threeletters.txt')
+    parser.add_argument('--outfile', default='defns.txt')
+    args = parser.parse_args()
+
+    config = ConfigParser.ConfigParser()
+    config.read('config.txt')
+
+    apiUrl = 'http://api.wordnik.com/v4'
+    apiKey = config.get('wordnik', 'api_key')
+    client = swagger.ApiClient(apiKey, apiUrl)
+
+    words = [l.strip().lower() for l in open(args.words_file)]
+
+    defns = {}
+    outfile = open(args.outfile, 'a')
+    writer = csv.writer(outfile)
+
+    wordApi = WordApi.WordApi(client)
+    for word in words:
+        if word < args.start_at:
+            continue
+
+        definitions = wordApi.getDefinitions(word, limit=2)
+                                             # partOfSpeech='verb',
+                                             # sourceDictionaries='wiktionary',
+        if definitions:
+            writer.writerow((word, definitions[0].text))
+        else:
+            writer.writerow((word, 'No definition found.\n'))
+        outfile.flush()
+        # print definitions
+        # print definitions[0].text
+        # print definitions[1].text
+        time.sleep(5)
